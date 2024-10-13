1b3b86960424f91e76ab46b548fbb8632e536b4c
JunXiong0223 <JunXiong0223@users.noreply.github.com>
2024-10-13 11:39:41 +0800

    Consolidate UI under JX

---
+++ JX/Payment Checkout/V2/assets/js/checkout.js
@@ -0,0 +1,118 @@
+let MemberPoint = 1000; // Replace with actual member point value
+
+let OnePoint = 0.01; // Replace with actual point value
+
+var pointAmount = 0; 
+var PromoCodeAmount = 0; 
+var TotalAmount = 0
+
+let carBattery = {
+    brand: "Exide",
+    model: "Exide Premium EA1000",
+    price: 129.99,
+    quantity: 3,
+    imageUrl: "assets/img/Car%20Battery.png" // Replace with actual image URL
+};
+
+
+
+document.getElementById("productName").innerHTML = carBattery.model;
+document.getElementById("productPrice").innerHTML = "RM" + carBattery.price;
+document.getElementById("productQuantity").innerHTML = carBattery.quantity;
+document.getElementById("productImage").src = carBattery.imageUrl;
+document.getElementById("productTotalPrice").innerHTML = "RM" + (carBattery.price * carBattery.quantity);
+document.getElementById('MemberPointValue').innerHTML = MemberPoint;
+
+document.getElementById('TotalProductsPrice').innerHTML = "RM" + (carBattery.price * carBattery.quantity);
+document.getElementById('TotalProductQuantity').innerHTML = carBattery.quantity;
+
+function calculateTotalAmount() {
+    TotalAmount = (carBattery.price * carBattery.quantity) - pointAmount - PromoCodeAmount;
+    //document.getElementById('TotalProductsPrice').innerHTML = "RM" + TotalAmount;
+    document.getElementById('TotalCheckoutAmount').innerHTML = "RM" + TotalAmount;
+    document.getElementById('FinalPrice').innerHTML = "RM" + TotalAmount;
+}
+
+function calculateTotalPrice() {
+    let totalPrice = carBattery.price * carBattery.quantity; // Replace with actual calculation logic
+
+    return totalPrice;
+}
+
+function usePoints(MemberPoint,OnePoint) {
+    pointAmount = MemberPoint * OnePoint;
+    return pointAmount;
+}
+
+function increaseQuantity(productID){
+    //let productClassInputID = `product${productID}-quantity`;
+    let product = document.getElementById(`product${productID}-quantity`);
+    let productValue = parseInt(product.value);
+    let newValue = productValue + 1;
+    product = newValue;
+    console.log(productID, productValue, newValue);
+    document.getElementById(`product${productID}-quantity`).value = product;
+}
+
+function decreaseQuantity(productID){
+    //let productClassInputID = `product${productID}-quantity`;
+    let product = document.getElementById(`product${productID}-quantity`);
+    let productValue = parseInt(product.value);
+    let newValue = productValue - 1;
+
+    if(newValue <= 0){
+        newValue = 0;
+    }
+
+    product = newValue;
+    console.log(productID, productValue, newValue);
+    document.getElementById(`product${productID}-quantity`).value = product;
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.getElementById('MemberPointChk').addEventListener('change', function() {
+        //const target = document.getElementById('MemberPoint');
+        if (this.checked) {
+            console.log('Checkbox is checked');
+            // Show the element
+            document.querySelectorAll('MemberPoint *').forEach(function(element) {
+                element.style.display = 'block'; // Resets the display property
+            });
+            
+            document.getElementById('MemberPoint').style.display = ''; 
+
+            usePoints(MemberPoint,OnePoint);
+            calculateTotalAmount();
+            document.getElementById('MemberPointConvert').innerHTML = "RM " + pointAmount;
+        }
+        else {
+            console.log('Checkbox is not checked');
+            // Hide the element
+            document.getElementById('MemberPoint').style.display = 'none';
+        }
+    });
+});
+
+const correctPromoCode = 'Code123';
+
+document.getElementById('PromoCodeBtn').addEventListener('click', function() {
+    const enteredCode = document.getElementById('promoCodeInput').value;
+     
+    if (enteredCode === correctPromoCode) {
+        document.getElementById('invalidPromoCodeMsg').style.display = 'none';
+        document.getElementById('PromoCodeBar').style.display = '';
+        PromoCodeAmount = 10; // Set the discount amount to 10
+
+        document.getElementById('PromoCodeValue').innerHTML = enteredCode; // Set the discount amount to 10
+        document.getElementById('PromoCodeConvert').innerHTML = "RM" + PromoCodeAmount;
+        calculateTotalAmount();
+    } else {
+        document.getElementById('invalidPromoCodeMsg').style.display = '';
+        document.getElementById('PromoCodeBar').style.display = 'none';
+    }
+});
+
+
+window.onload = calculateTotalAmount();
---
+++ JX/Profile/V2/assets/js/untitled.js
@@ -0,0 +1,133 @@
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.changePasswordForm');
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        // Prevent submission if the form is invalid
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          const password = document.getElementById('NewPassword').value;
+          const confirmPassword = document.getElementById('ConfirmNewPassword').value;
+  
+          // Compare passwords and handle mismatch
+          if (!comparePasswords(password, confirmPassword)) {
+            alert('Password not Match');
+            
+            var errMes = document.getElementById('ErrorMes');
+            errMes.classList.remove('invalid-feedback');
+            errMes.classList.add('valid-feedback');
+
+            var MisPass = document.getElementById('MisMatchPassword');
+            MisPass.classList.remove('invisible');
+            MisPass.classList.add('invalid-feedback');
+                
+            var Message = document.getElementById('ConfirmNewPassword');
+            if(Message.classList.contains('is-valid')){
+              Message.classList.remove('is-valid');
+            }
+            Message.classList.add('is-invalid')
+            
+            if(form.classList.contains('was-validated')){
+              form.classList.remove('was-validated');
+            }
+
+            event.preventDefault(); // Prevent form submission if passwords don't match
+            event.stopPropagation();
+
+            return;
+          }
+        }
+        
+        var errMes = document.getElementById('ErrorMes');
+        //errMes.classList.add('invisible');
+        if (errMes.classList.contains('valid-feedback')){
+          errMes.classList.remove('valid-feedback');
+        }
+        if (errMes.classList.contains('invalid-feedback') === false)
+        {
+          errMes.classList.add('invalid-feedback')
+        }
+
+        var MisPass = document.getElementById('MisMatchPassword');
+        MisPass.classList.add('invisible');
+
+        var Message = document.getElementById('ConfirmNewPassword');
+        if (Message.classList.contains('is-invalid')){
+          Message.classList.remove('is-invalid');
+        }
+        Message.classList.add('is-valid');
+        
+
+        form.classList.add('was-validated');
+        
+        
+      }, false);
+    });
+    
+})()
+
+function comparePasswords(password, confirmPassword) {
+
+
+  // Check if passwords match
+  if (password !== confirmPassword) {
+      return false;
+  }
+
+  return true;
+}
+
+function EditProfileName()
+{
+  var Username = document.getElementById('Username');
+  var email = document.getElementById('email');
+
+  if (Username.disabled && email.disabled)
+  {
+    Username.disabled = false;
+    email.disabled = false;
+  }
+  else
+  {
+    Username.disabled = true;
+    email.disabled = true;
+  }
+
+}
+
+function EditShippingAddress()
+{
+  var address = document.getElementById('shippingAddress');
+
+  if (address.disabled)
+  {
+    address.disabled = false;
+  }
+  else
+  {
+    address.disabled = true;
+  }
+
+}
+
+function EditPhoneNO()
+{
+  var PhoneNO = document.getElementById('phoneNumber');
+
+  if (PhoneNO.disabled)
+  {
+    PhoneNO.disabled = false;
+  }
+  else
+  {
+    PhoneNO.disabled = true;
+  }
+
+}
