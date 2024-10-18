cbc3d081510500b095ba70c0713433ec62ee3701
JunXiong0223 <JunXiong0223@users.noreply.github.com>
2024-10-18 22:50:19 +0800

    rename

---
+++ Fooasdgfghter.php
@@ -0,0 +1,35 @@
+<footer class="text-white bg-dark">
+    <div class="container py-4 py-lg-5">
+        <div class="row justify-content-center">
+            <div class="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column">
+                <h3 class="fs-4 text-white"><strong>Contact Us</strong></h3>
+                <ul class="list-unstyled">
+                    <li><a class="link-light" href="#"><i class="fas fa-phone-alt" style="margin-right: 5px;"></i>03-2288-2888</a></li>
+                    <li><a class="link-light" href="#"><i class="fas fa-mail-bulk" style="margin-right: 5px;"></i>support@amaron.com</a></li>
+                    <li><a class="link-light" href="#"><i class="fab fa-whatsapp" style="margin-right: 5px;"></i>012345678/WhatsApp.com</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column">
+                <h3 class="fs-4 text-white"><strong>Find Us</strong></h3>
+                <ul class="list-unstyled">
+                    <li><a class="link-light" href="#"><i class="fas fa-location-arrow" style="margin-right: 5px;"></i>Seberang Perai</a></li>
+                    <li><a class="link-light" href="#"><i class="fas fa-location-arrow" style="margin-right: 5px;"></i>Bayan Lepas</a></li>
+                    <li><a class="link-light" href="#"><i class="fas fa-location-arrow" style="margin-right: 5px;"></i>Jelutong</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-4 col-md-3 text-center text-lg-start d-flex flex-column">
+                <h3 class="fs-4 text-white"><strong>Customer Care</strong></h3>
+                <ul class="list-unstyled">
+                    <li><a class="link-light" href="#">Warranty</a></li>
+                    <li><a class="link-light" href="#">Payment</a></li>
+                    <li><a class="link-light" href="#">Operation Hours</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 text-center text-lg-start d-flex flex-column align-items-center order-first align-items-lg-start order-lg-last">
+                <h3 class="fs-4 text-white"><strong>Brands</strong></h3><img src="assets/img/Amaron%20header.jpg" width="63" height="64">
+                <figure class="figure"></figure>
+            </div>
+        </div>
+        <hr>
+    </div>
+</footer>
